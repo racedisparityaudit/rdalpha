c8b2042454238b1803a898ae8a67588d79b282eb
CalumJEadie <calum@calumjeadie.com>
2017-01-27 11:39:06 +0000

    Very basic setup of templates, js and css

--- web/app/urls.py
+++ web/app/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^a$', TemplateView.as_view(template_name='a.html'), name='a'),
+    url(r'^b$', TemplateView.as_view(template_name='b.html'), name='b'),
+    url(r'^c$', TemplateView.as_view(template_name='c.html'), name='c'),
 ]
