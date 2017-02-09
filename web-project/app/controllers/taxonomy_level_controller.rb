322e0681076ab55b95689894b1322ca3c3139b8e
Kane Morgan <kane.a.s.morgan@gmail.com>
2017-02-09 16:01:47 +0000

    final fixes

--- web-project/app/controllers/taxonomy_level_controller.rb
+++ web-project/app/controllers/taxonomy_level_controller.rb
@@ -6,6 +6,7 @@ class TaxonomyLevelController < ApplicationController
       @taxonomy_level.uri ==  "/housingandlivingstandards/socialandaffordablehousing/accesstosocialhousing/housingandliving16" ? render('question') : render('missing')
     else
       @topics  = TaxonomyLevel.find_by_name(params[:taxonomy_name]).taxonomy_levels
+                    .reject { |level| level.name == @taxonomy_level.name }
     end
   end
 
