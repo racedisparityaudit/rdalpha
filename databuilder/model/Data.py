838110b9bc18cb9ba6c798fdd22aa678a959a9da
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2017-02-15 21:52:02 +0000

    Complicated data importer

--- databuilder/model/Data.py
+++ databuilder/model/Data.py
@@ -6,7 +6,21 @@ class DataPoint(object):
 
 class DataSeries(object):
 
-    def __init__(self, value_column, category_columns):
-        self.value_column = value_column
+    def __init__(self, measure, category_columns):
+        self.measure = measure
         self.category_columns = category_columns
         self.points = []
+
+    def find(self, filter) -> DataPoint:
+        match_values = list(filter.values())
+        match_indices = [self.category_columns.index(key) for key in filter.keys()]
+        for point in self.points:
+            if self.check_point(point, match_indices, match_values):
+                return point
+
+    def check_point(self, point, match_indices, match_values):
+        for i in range(0, len(match_indices)):
+            point_value = point.categories[match_indices[i]]
+            if point_value != match_values[i]:
+                return False
+        return True
--- databuilder/tests/test_measureImporter.py
+++ databuilder/tests/test_measureImporter.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest import TestCase
-from databuilder.taskrunners.data.MeasureImporter import MeasureImporter
+from databuilder.taskrunners.data.SimpleMeasureImporter import SimpleMeasureImporter, XYMeasureListImporter
 
 class TestMeasureImporter(TestCase):
 
@@ -9,7 +9,7 @@ class TestMeasureImporter(TestCase):
         header_row = ['alpha', 'bravo', 'charlie', 'delta', 'echo']
 
         # When
-        importer = MeasureImporter()
+        importer = SimpleMeasureImporter()
         delta_index = importer.find_column(header_row, 'delta')
 
         # Then
@@ -20,21 +20,21 @@ class TestMeasureImporter(TestCase):
         header_row = ['alpha', 'bravo', 'charlie', 'delta', 'echo']
 
         # When
-        importer = MeasureImporter()
+        importer = SimpleMeasureImporter()
         column_indices = importer.find_category_indexes(header_row, ['bravo', 'delta'])
 
         # Then
         self.assertEqual(column_indices[0], 1)
         self.assertEqual(column_indices[1], 3)
 
-    def test_import_series(self):
+    def test_import_simple_measure(self):
         # Given
         file = "test_data/example_data.csv"
         categories = ["Region_name","LA_name"]
         value = 'WHITE_LADEN_16'
 
         # When
-        importer = MeasureImporter()
+        importer = SimpleMeasureImporter()
         series = importer.import_series(file=file, category_columns=categories, value_column=value)
 
         # Then
@@ -44,6 +44,27 @@ class TestMeasureImporter(TestCase):
         self.assertEqual(point.value, '1810')
         self.assertEqual(point.categories[0], 'North East')
         self.assertEqual(point.categories[1], 'Gateshead')
+
+    def test_import_xy_sheet(self):
+        #Given
+        file = "test_data/example_data_2.csv"
+        row_categories = ['Gender', 'Ethnicity']
+        column_categories = ['Region_name', 'LA_name']
+
+        # When
+        importer = XYMeasureListImporter()
+        sheet = importer.import_spreadsheet(file=file,
+                                            category_column_headers=column_categories,
+                                            category_row_names=row_categories)
+
+        # Then
+        series = sheet[0];
+        self.assertEqual(series.measure, "Pupil Count")
+        point = series.find({"Gender":"Male",
+                             "Ethnicity":"White",
+                             "Region_name":"North East",
+                             "LA_name":"Gateshead"})
+        self.assertEqual(point.value, '949')
 
 if __name__ == '__main__':
     unittest.main()
