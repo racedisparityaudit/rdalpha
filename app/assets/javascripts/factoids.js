1b99199b18e2b3946ca2880b06da7b42bc31f8cb
Kane Morgan <kane.a.s.morgan@gmail.com>
2017-02-16 14:23:39 +0000

    use self calling functions to create namespaces for js - stops conflicts when its loaded server side

--- app/assets/javascripts/factoids.js
+++ app/assets/javascripts/factoids.js
@@ -1,33 +1,29 @@
-function init(){
+(function(){
+
+  function initFactoids(){
 
 
-  console.log($("button"))
-  function applyFilters(e){
-    e.preventDefault()
-    var selectedTopics = $("input",".options-container").filter(function(i,e){ return e.checked})
-    $('.row-container').hide()
-      selectedTopics.each(function(i,topic){
-        console.log(topic)
-      var klass = "." + topic.name.split(" ")[0]
-      $(klass).show()
+    console.log($("button"))
+    function applyFilters(e){
+      e.preventDefault()
+      var selectedTopics = $("input",".options-container").filter(function(i,e){ return e.checked})
+      $('.row-container').hide()
+        selectedTopics.each(function(i,topic){
+          console.log(topic)
+        var klass = "." + topic.name.split(" ")[0]
+        $(klass).show()
+      })
+
+    }
+
+    $("button").on("click",applyFilters)
+    //TODO remove hack - should just work with frontend toolkit
+    $(".selection-button-radio").click(function(e){
+      $(".selection-button-radio").removeClass("selected")
+      $(e.currentTarget).addClass("selected")
     })
-
   }
 
-  $("button").on("click",applyFilters)
-  //TODO remove hack - should just work with frontend toolkit
-  $(".selection-button-radio").click(function(e){
-    $(".selection-button-radio").removeClass("selected")
-    $(e.currentTarget).addClass("selected")
-  })
-}
-
-$(document).ready(init)
-$(document).on('turbolinks:load', init)
-
-function test(){
-  console.log("test")
-}
-
-$(document).ready(test)
-$(document).on('turbolinks:load', test)
+  $(document).ready(initFactoids)
+  $(document).on('turbolinks:load', initFactoids)
+}())
