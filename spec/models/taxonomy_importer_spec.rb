7ac0a0c80590d91d2af8bc7dc3cb808385b2c42b
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2017-02-27 16:47:52 +0000

    Taxonomy Importer now imports the new models from the csv

--- spec/models/taxonomy_importer_spec.rb
+++ spec/models/taxonomy_importer_spec.rb
@@ -2,14 +2,52 @@ require 'rails_helper'
 
 RSpec.describe TaxonomyImporter, type: :model do
 
-  it "should import measure averages" do
-
-    #TODO: finish writing this rspec wasn't working KASM
-    path = Rails.root.join("/spec/fixtures/measure_averages.csv").to_s
-
-    TaxonomyImporter.import(path)
-
-    expect(MeasureAverage.count).not_to be(0)
+  before(:each) do
+    @importer = TaxonomyImporter.new
   end
 
+  it "should import rows from the csvs" do
+    @importer.import
+
+    expect(MeasureAverage.count).not_to be(0)
+    expect(Topic.count).not_to be(0)
+    expect(Measure.count).not_to be(0)
+  end
+
+
+  it "should import all measures" do
+    require 'csv'
+    total_measures_in_csv =
+      CSV.read(Rails.root.join("./data/output/taxonomy.csv"),  encoding: "ISO8859-1:utf-8")
+        .select{|r| r[5] == "T4"}.count
+    @importer.import_measures
+    expect(total_measures_in_csv).to eql(Measure.count)
+  end
+
+  it "should import measures with the correct topic" do
+    pending "to be implemented..."
+  end
+
+  it " should delete old data on import" do
+    @importer.import
+    counts = [ MeasureAverage.count, Topic.count, Measure.count]
+
+    @importer.import
+    new_counts = [ MeasureAverage.count, Topic.count, Measure.count]
+
+    new_counts.each_with_index do |count, index|
+      expect(counts[index]).to eql count
+    end
+  end
+
+  it "should import topics" do
+    require 'csv'
+    total_topics_in_csv =
+      CSV.read(Rails.root.join("./data/output/taxonomy.csv"),  encoding: "ISO8859-1:utf-8")
+        .tap{ |t| t.shift }
+        .reject{|r| r[5] == "T4"}.count
+    @importer.import_topics
+    binding.pry
+    expect(total_topics_in_csv).to eql(Topic.count)
+  end
 end
