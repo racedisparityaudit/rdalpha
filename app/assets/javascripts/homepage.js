a0b68b8974cf0aefa1aa33e04af741e67c33ffe9
Kane Morgan <kane.a.s.morgan@gmail.com>
2017-02-27 10:20:49 +0000

    install rspec

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,5 +1,20 @@
 (function(){
 
-  new GOVUK.SelectionButtons();
+  function init(){
+    new GOVUK.SelectionButtons();
 
+    $(".topic-tabs li").on("click", function(e) {
+        e.preventDefault()
+        $(".graph-section").hide()
+        var section_id = "#" + this.id.split("-")[0] + "-section"
+        $(section_id).toggle()
+        console.log(section_id)}
+      )
+      $("#ethnicity-selector").trigger("click")
+
+    }
+  }
+
+  $(document).ready(init)
+  $(document).on('turbolinks:load', init)
 }())
