f9ee606c426bdcb0f1e6764c67027dbcd4211cc6
Tom Ridd <tom.ridd@methodsdigital.co.uk>
2017-03-02 17:57:52 +0000

    Measure object to store a specific measure

---
+++ databuilder/model/Measure.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+class DimensionList(object):
+    def __init__(self, race_type = "ONS 2011 5", location_type = "National",
+                 income_type = "Household Income", time_type = "Year", other = []):
+        self.race_type = race_type
+        self.location_type = location_type
+        self.income_type = income_type
+        self.time_type = time_type
+        self.other = other
+
+    def columns(self):
+        return ['Race','Location','Income','Time'] + self.other
+
+class Measure(object):
+
+    def __init__(self, dimensions: DimensionList):
+        self.dimensions = dimensions
+        self.values = pd.DataFrame(columns=self.dimensions.columns())
+
