0b47677a1968b9a207d38197664a4eccf3017947
Kane Morgan <kane.a.s.morgan@gmail.com>
2017-03-21 17:00:39 +0000

    open all button for accordions

--- app/assets/javascripts/measure.js
+++ app/assets/javascripts/measure.js
@@ -3,12 +3,33 @@
   var initialised = false;
 
   function accordions(){
+    var accordionsAllOpen = false;
+
     $(".accordion__header").click(function(e){
         var body = $(e.currentTarget).parent().find(".accordion__body")
         $(e.currentTarget).find(".plus-minus-icon").toggleClass("open")
         $(body).toggle()
     })
     $(".accordion__body").hide()
+
+    $("#accordion-all-control").click(function(){
+    a= $(".plus-minus-icon").filter(function(_,icon) {
+        console.log(icon)
+        return icon.classList.contains("open")
+      })
+
+      if(a.size() == 0){
+        console.log("called")
+        $(".plus-minus-icon").addClass("open")
+        $("#accordion-all-control").text("Close all")
+        $(".accordion__body").show()
+      } else {
+        $(".plus-minus-icon").removeClass("open")
+        $("#accordion-all-control").text("Open all")
+        $(".accordion__body").hide()
+      }
+      console.log(a)
+    })
   }
 
   $(document).on('turbolinks:load', accordions)
