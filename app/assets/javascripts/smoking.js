5de59081df562e6526d7ec44ab653b82bf43c3dc
Kane Morgan <kane.a.s.morgan@gmail.com>
2017-03-23 08:58:02 +0000

    add tabs to smoking page

--- app/assets/javascripts/smoking.js
+++ app/assets/javascripts/smoking.js
@@ -19,7 +19,72 @@
         yAxis: {
             min: 0,
             title: {
-                text: 'Unemployment rate',
+                text: 'Smoking prevalence',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify',
+                formatter: function () {
+                    return (100 * this.value).toFixed(1) + "%";
+                }
+            }
+        },
+        tooltip: {
+            formatter: function () {
+                        if (isNaN(this.y)) {
+                            return "We don't have enough data";
+                        } else {
+                            return '<b>' + this.key + ': ' + (100 * this.point.y).toFixed(1) + '</b>% <br>';
+                        }
+                    }
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true,
+                    formatter: function () {
+                        if (isNaN(this.y)) {
+                            return "";
+                        } else {
+                            return '<b>' + (100 * this.y).toFixed(1) + '</b>% <br>';
+                        }
+                    }
+                }
+            }
+        },
+        legend: {
+            enabled:false
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'All',
+            data: [0.092, 0.090, 0.026, 0.0024, null, 0.029, null ]
+        }]
+    });
+  }
+
+  function drawRegularChart(){
+    Highcharts.chart('regular', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Smoking prevalence at age 15'
+        },
+        xAxis: {
+            categories: ["White", "Mixed", "Asian", "Black",
+            "Chinese", "Other",
+            "Unknown"],
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Smoking prevalence',
                 align: 'high'
             },
             labels: {
@@ -63,8 +128,72 @@
             data: [0.062, 0.054, 0.015, 0.009, null, 0.022, null ]
         }]
     });
-}
+  }
 
+  function drawOccasionalChart(){
+    Highcharts.chart('occasional', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Smoking prevalence at age 15'
+        },
+        xAxis: {
+            categories: ["White", "Mixed", "Asian", "Black",
+            "Chinese", "Other",
+            "Unknown"],
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Smoking prevalence',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify',
+                formatter: function () {
+                    return (100 * this.value).toFixed(1) + "%";
+                }
+            }
+        },
+        tooltip: {
+            formatter: function () {
+                        if (isNaN(this.y)) {
+                            return "We don't have enough data";
+                        } else {
+                            return '<b>' + this.key + ': ' + (100 * this.point.y).toFixed(1) + '</b>% <br>';
+                        }
+                    }
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true,
+                    formatter: function () {
+                        if (isNaN(this.y)) {
+                            return "";
+                        } else {
+                            return '<b>' + (100 * this.y).toFixed(1) + '</b>% <br>';
+                        }
+                    }
+                }
+            }
+        },
+        legend: {
+            enabled:false
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'All',
+            data: [0.030, 0.037, 0.010, 0.015, null, 0.007, null ]
+        }]
+    });
+  }
 
 
 
@@ -74,9 +203,10 @@ function drawCharts(){
     }
     console.log("drawing smoking charts");
     drawOverallChart();
-    $("#ethnicity-section").show();
+    drawRegularChart();
+    drawOccasionalChart();
+    $("#current-selector").trigger("click");
 }
-
     $(document).ready(drawCharts)
     $(document).on('turbolinks:load', drawCharts)
 }())
